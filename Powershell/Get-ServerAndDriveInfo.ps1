b1e89fb1e29111cc5c420a1141842b7e158ac117
TJ Norred <tjnorred@gmail.com>
2020-02-06 11:22:24 -0500

    Create Get-ServerAndDriveInfo.ps1

---
+++ Powershell/Get-ServerAndDriveInfo.ps1
@@ -0,0 +1,4 @@
+﻿Invoke-command -ComputerName <server_name_here> -ScriptBlock { Get-CimInstance Win32_OperatingSystem | Select-Object CSName, Caption, Version | fl;
+ get-psdrive -PSProvider FileSystem; Get-WmiObject -Class Win32_logicaldisk -Filter "DriveType = '3'" | Select-Object DeviceID, VolumeName | fl;
+ Get-WmiObject -Class Win32_logicaldisk -Filter "DriveType = '4'" | Select-Object DeviceID, VolumeName | fl;
+  } -Credential <Dem_Creds>
